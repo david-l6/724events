1ad8a72f28fde4705e227735bb3eb888041f186b
david-l6 <github.decimal773@passmail.net>
2024-07-22 14:33:38 +0200

    bug home corriger

--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import Home from "./index";
 
 describe("When Form is created", () => {
@@ -20,8 +20,9 @@ describe("When Form is created", () => {
           bubbles: true,
         })
       );
-      await screen.findByText("En cours");
-      await screen.findByText("Message envoyé !");
+      await waitFor(() => {
+        expect(screen.getByText("Message envoyé !")).toBeInTheDocument();
+      });
     });
   });
 
